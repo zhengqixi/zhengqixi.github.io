cefbbd17246c707505a1cd5f054471fe2eff70c0
zxi <zxi8@bloomberg.net>
2018-03-07 16:04:24 -0500

    This setup should work

---
+++ cv.js
@@ -0,0 +1,26 @@
+var format = "resume";
+function data() {
+    let data = 
+    {
+    "header": {},
+    "experience": [],
+    "skills": []
+   };
+   return data;
+}
+
+function cv() {
+
+}
+
+function resume() {
+
+}
+
+function toggleFormat() {
+    dumpToConsole();
+}
+
+function dumpToConsole() {
+    console.log(data());
+}
